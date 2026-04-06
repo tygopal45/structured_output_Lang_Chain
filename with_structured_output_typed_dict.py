db692af36564126ec8d3b93898f5a0e1d5bb4c9d
tygopal45 <jeegopal835@gmail.com>
2026-04-06 22:52:57 +0530

    structured output with typed dict

---
+++ with_structured_output_typed_dict.py
@@ -0,0 +1,20 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from typing import TypedDict
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+# schema
+
+class Review(TypedDict):
+    summary: str
+    sentiment: str
+
+
+structured_model = model.with_structured_output(Review) # type: ignore
+
+result = structured_model.invoke("""The hardware is great, but the software feels bloated. There are too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to other brands. Hoping for a software update to fix this.""")
+
+print(result)
